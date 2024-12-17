e099540052922de6638affeea3584a6237124252
merd <oyunkafasimertcan@gmail.com>
2024-12-17 20:52:10 +0300

    mobil senkronizasyon hafiften yapıldı eklemeler yapılabilir ve ana sayfaya resim eklendi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector("#menu-btn");
+
+const navbar = document.querySelector(".navbar");
+
+menuBtn.addEventListener("click", function() {
+    navbar.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(
+            !e.composedPath().includes(menuBtn) && 
+            !e.composedPath().includes(navbar)
+        ) {
+            navbar.classList.remove("active");
+        }
+    })
+})
